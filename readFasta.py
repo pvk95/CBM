1e5bc1d9f7dfc774756a0eacf46a5bc2f73f0e4f
Hana Parizkova <hanap@student.ethz.ch>
2018-10-26 19:03:58 +0200

    Upload New File

---
+++ readFasta.py
@@ -0,0 +1,27 @@
+import gzip
+
+def readFasta(filename,verbose=0):
+    """ Parses a classically formatted and possibly 
+        compressed FASTA file into a dictionary where the key
+        for a sequence is the first part of its header without 
+        any white space; if verbose is nonzero then the identifiers 
+        together with lengths of the read sequences are printed"""
+    if (filename.endswith(".gz")):
+        fp = gzip.open(filename, 'r')
+    else:
+        fp = open(filename, 'r')
+    # split at headers
+    data = fp.read().split('>')
+    fp.close()
+    # ignore whatever appears before the 1st header
+    data.pop(0)     
+    # prepare the dictionary
+    D = {}
+    for sequence in data:
+        lines = sequence.split('\n')
+        header = lines.pop(0).split()
+        key = header[0]
+        D[key] = ''.join(lines)
+        if verbose:
+            print("Sequence %s of length %d read" % (key,len(D[key])))
+    return D
