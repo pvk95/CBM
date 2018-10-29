1480eebfd98da6e5a16a61f3349d62bcee66f2a0
Brynja Sigurpalsdottir <brynjas@student.ethz.ch>
2018-10-29 16:29:49 +0100

    Update output_SAM.py

--- output_SAM.py
+++ output_SAM.py
@@ -5,50 +5,17 @@ Created on Tue Oct 23 17:07:29 2018
 
 @author: brynja
 """
-
-"""
-    Input: 
-        Dictionary with
-        key: qname = read id
-        values: [seq = sequence
-                f1 = bitwise flag reporting whether it's coming form the reverse
-                    string or the forward string
-                f2 = bitwise flag reporting secondary alignments - all false
-                rname: reference name (same for everything in our case)
-                pos: left most position of the alignment
-                CIGAR = cigar string]
+import csv  
     
-    Output:
-        Sam file with the following header:
-            qname seq f1 f2 rname pos CIGAR    
-"""
-import csv
-
-def output_SAM(outfile, d):
-    # USING NO LIBRARIES
-    header = ['read id', 'seq', 'reverse', 'secondary_alignment', 'reference',
-              'pos', 'cigarstring'] 
-    with open(outfile, 'w') as o:
-        o.write('\t'.join(header))
-        o.write('\n')
-        for key, value in d.items():
-            o.write(key + '\t')
-            for item in value:
-                o.write(str(item))
-                o.write('\t')
-            o.write('\n')
-    o.close()
-    
-
-def output_SAM2(outfile, d):
-    # USING CSV LIBRARY
-    header = ['read id', 'seq', 'reverse', 'secondary_alignment', 'reference',
-              'pos', 'cigarstring'] 
+def output_SAM(outfile, header, d):
     with open(outfile, "w") as o:
         writer = csv.writer(o, delimiter='\t')
-        writer.writerow(header)
+        o.write(''.join(header))
         for key, value in d.items():
-            writer.writerow([key, value[0], value[1], value[2], value[3], value[4], value[5]])
-            # If all values are strings we can also to '\t'.join(value) and replace       
+            if len(value) == 1:
+                writer.writerow([key, 4, '*', 0, 0, '*', '*', 0, 0, '*', 0])
+            else:
+                if value[1] == 1:
+                    value[1] = 16
+                writer.writerow([key, value[1], value[3], value[4], 0, value[5], '*',0, 0, value[0], '*'])
     o.close()
-    
